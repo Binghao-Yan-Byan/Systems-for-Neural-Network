0130fc9d5953c03780bdcbf7ed6bedca3e54c956
Binghao Yan <byan@wm.edu>
2024-12-01 11:21:55 -0500

    update

--- LeNetTrain_DDP_Byan.py
+++ LeNetTrain_DDP_Byan.py
@@ -10,28 +10,37 @@ from LeNet_Byan import LeNet_Byan
 from torchvision import datasets, transforms
 
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
-transform = transforms.Compose([transforms.ToTensor()])
-train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+
 
 test_dataset = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
-
-def train(rank, world_size):
+def setup_ddp(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '23456'
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
-    torch.cuda_set_device(rank)
-    device = rank
+
+def cleanup_ddp():
+    dist.destroy_process_group()
+
+def ddp_train(rank, world_size):
+    setup_ddp(rank, world_size)
+
+    device = torch.device(f'cuda:{rank}')
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+    sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=64, sampler=sampler)
+
     model = LeNet_Byan(device)
     model = DDP(model, device_ids=[rank])
     epochs = 20
     optimizer = optim.SGD(model.parameters(), lr=0.01)
     criterion = nn.CrossEntropyLoss()
 
-    sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
-    train_loader = DataLoader(train_dataset, batch_size=64, sampler=sampler)
-
     for epoch in range(epochs):
         sampler.set_epoch(epoch)
+        model.train()
         loss = 0.0
         for data, target in train_loader:
             data, target = data.to(device), target.to(device)
@@ -65,7 +74,7 @@ def test(rank, model, test_loader, device):
 
 def main():
     world_size = 2
-    mp.spawn(train, args=(world_size,), nprocs=world_size, join=True)
+    mp.spawn(ddp_train, args=(world_size,), nprocs=world_size, join=True)
 
 if __name__ == "__main__":
     main()
