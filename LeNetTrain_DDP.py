73968dae60cb7e6ff251187b889d3d06370ee37e
Binghao Yan <byan@wm.edu>
2024-12-01 11:45:17 -0500

    update

--- LeNetTrain_DDP.py
+++ LeNetTrain_DDP.py
@@ -8,29 +8,7 @@ import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, DistributedSampler
-
-# Define the LeNet model
-class LeNet(nn.Module):
-    def __init__(self):
-        super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2)
-        self.relu = nn.ReLU()
-        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.pool(x)
-        x = self.relu(self.conv2(x))
-        x = self.pool(x)
-        x = x.view(-1, 16 * 5 * 5)
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+from LeNet import LeNet300100
 
 # Setup and cleanup functions for DDP
 def setup_ddp(rank, world_size):
@@ -84,7 +62,7 @@ def ddp_main(rank, world_size):
     dataloader = DataLoader(dataset, batch_size=64, sampler=sampler)
 
     # Model, loss function, and optimizer
-    model = LeNet().to(device)
+    model = LeNet300100().to(device)
     model = DDP(model, device_ids=[rank])
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
